f4b82c49a22639f40e01c8c8bfb71e36bcd8e314
Rider <riderjensen@gmail.com>
2018-11-12 15:43:54 -0700

    Deleting bad work

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -69,24 +69,10 @@ export default {
 			this.ourColor = incColor;
 		},
 		exportImg() {
-			// const {body} = document
-
-			// const canvas = document.createElement('canvas')
-			// const ctx = canvas.getContext('2d')
-			// canvas.width = canvas.height = 100
-
-			// const tempImg = document.createElement('img')
-			// tempImg.addEventListener('load', onTempImageLoad)
-			// let ourTest = document.getElementById("myTestHere").outerHTML;
-			// tempImg.src = 'data:image/svg+xml,' + encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">'+ourTest+'</div></foreignObject></svg>')
-
-			// const targetImg = document.createElement('img')
-			// body.appendChild(targetImg)
-
-			// function onTempImageLoad(e){
-			// ctx.drawImage(e.target, 0, 0)
-			// targetImg.src = canvas.toDataURL()
-			html2canvas(document.querySelector("#myTestHere")).then(function(canvas) {
+			// remove styling on each pixel block and 
+			html2canvas(document.querySelector("#myTestHere"), {
+				canvas: null
+			}).then(function(canvas) {
     			document.body.appendChild(canvas);
 			})
 			
